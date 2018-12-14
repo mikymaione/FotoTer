853f24ad963a81b0811a110b1c6ed5fa51bcf5d2
Michele Maione <mikymaione@hotmail.it>
2018-12-14 19:44:07 +0100

    first commit

---
+++ FotoTeR/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.eFrom = New System.Windows.Forms.TextBox()
+        Me.eTo = New System.Windows.Forms.TextBox()
+        Me.eRename = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.bFrom = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.bVai = New System.Windows.Forms.Button()
+        Me.bTo = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Trasferisci da"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Trasferisci a"
+        '
+        'eFrom
+        '
+        Me.eFrom.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.eFrom.Location = New System.Drawing.Point(15, 25)
+        Me.eFrom.Name = "eFrom"
+        Me.eFrom.ReadOnly = True
+        Me.eFrom.Size = New System.Drawing.Size(262, 20)
+        Me.eFrom.TabIndex = 0
+        '
+        'eTo
+        '
+        Me.eTo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.eTo.Location = New System.Drawing.Point(15, 74)
+        Me.eTo.Name = "eTo"
+        Me.eTo.ReadOnly = True
+        Me.eTo.Size = New System.Drawing.Size(262, 20)
+        Me.eTo.TabIndex = 2
+        '
+        'eRename
+        '
+        Me.eRename.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.eRename.Location = New System.Drawing.Point(15, 123)
+        Me.eRename.Name = "eRename"
+        Me.eRename.Size = New System.Drawing.Size(293, 20)
+        Me.eRename.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Rinomina in"
+        '
+        'bFrom
+        '
+        Me.bFrom.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bFrom.Image = Global.FotoTeR.My.Resources.Resources.folder_picture
+        Me.bFrom.Location = New System.Drawing.Point(283, 23)
+        Me.bFrom.Name = "bFrom"
+        Me.bFrom.Size = New System.Drawing.Size(25, 25)
+        Me.bFrom.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.bFrom, "Scegli cartella ...")
+        Me.bFrom.UseVisualStyleBackColor = True
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.IsBalloon = True
+        Me.ToolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.ToolTip1.ToolTipTitle = "Informazione"
+        '
+        'bVai
+        '
+        Me.bVai.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bVai.Image = Global.FotoTeR.My.Resources.Resources.accept
+        Me.bVai.Location = New System.Drawing.Point(252, 164)
+        Me.bVai.Name = "bVai"
+        Me.bVai.Size = New System.Drawing.Size(56, 25)
+        Me.bVai.TabIndex = 5
+        Me.bVai.Text = "Vai"
+        Me.bVai.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.ToolTip1.SetToolTip(Me.bVai, "Inizia il processo di trasferimento e rinomina")
+        Me.bVai.UseVisualStyleBackColor = True
+        '
+        'bTo
+        '
+        Me.bTo.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bTo.Image = Global.FotoTeR.My.Resources.Resources.folder_picture
+        Me.bTo.Location = New System.Drawing.Point(283, 72)
+        Me.bTo.Name = "bTo"
+        Me.bTo.Size = New System.Drawing.Size(25, 25)
+        Me.bTo.TabIndex = 3
+        Me.ToolTip1.SetToolTip(Me.bTo, "Scegli cartella ...")
+        Me.bTo.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 164)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(231, 25)
+        Me.ProgressBar1.TabIndex = 6
+        Me.ProgressBar1.Visible = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.bVai
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(319, 196)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.bVai)
+        Me.Controls.Add(Me.bTo)
+        Me.Controls.Add(Me.bFrom)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.eRename)
+        Me.Controls.Add(Me.eTo)
+        Me.Controls.Add(Me.eFrom)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(800, 235)
+        Me.MinimumSize = New System.Drawing.Size(335, 235)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FotoTeR"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents eFrom As System.Windows.Forms.TextBox
+    Friend WithEvents eTo As System.Windows.Forms.TextBox
+    Friend WithEvents eRename As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents bFrom As System.Windows.Forms.Button
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents bTo As System.Windows.Forms.Button
+    Friend WithEvents bVai As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+
+End Class
---
+++ FotoTeR/Form1.vb
@@ -0,0 +1,250 @@
+﻿'MIT License
+'Copyright(c) 2018 Michele Maione
+'Permission Is hereby granted, free Of charge, to any person obtaining a copy of this software And associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell copies of the Software, And to permit persons to whom the Software Is furnished to do so, subject to the following conditions: The above copyright notice And this permission notice shall be included In all copies Or substantial portions Of the Software.
+'THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY, FITNESS For A PARTICULAR PURPOSE And NONINFRINGEMENT. In NO Event SHALL THE AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM, OUT Of Or In CONNECTION With THE SOFTWARE Or THE USE Or OTHER DEALINGS In THE SOFTWARE.
+Public Class Form1
+
+    <Serializable()>
+    Structure sOpzioni
+        Dim From_, To_ As String
+
+        Sub New(ByVal From__ As String, ByVal To__ As String)
+            From_ = From__
+            To_ = To__
+        End Sub
+    End Structure
+
+    Structure TipoFile
+        Dim Tipo As String
+        Dim Numero As Integer
+
+        Sub New(ByVal Tipo_ As String)
+            Tipo = Tipo_
+            Numero = 0
+        End Sub
+    End Structure
+
+
+    Public Property Opzioni As sOpzioni
+        Get
+            Dim s = IO.Path.Combine(IO.Directory.GetParent(Application.UserAppDataPath).FullName, "\..\FotoTeR\Opzioni.db")
+
+            If IO.File.Exists(s) Then
+                Using fs As New IO.FileStream(s, IO.FileMode.Open)
+                    Dim bf As New Runtime.Serialization.Formatters.Binary.BinaryFormatter
+
+                    Return bf.Deserialize(fs)
+                End Using
+            End If
+
+            Return New sOpzioni("", "")
+        End Get
+        Set(ByVal value As sOpzioni)
+            Dim s As String = IO.Directory.GetParent(Application.UserAppDataPath).FullName + "\..\FotoTeR\Opzioni.db"
+
+            Using fs As New IO.FileStream(s, IO.FileMode.OpenOrCreate)
+                Dim bf As New Runtime.Serialization.Formatters.Binary.BinaryFormatter
+                bf.Serialize(fs, value)
+            End Using
+        End Set
+    End Property
+
+
+    Private Sub bVai_Click(ByVal sender As Object, ByVal e As EventArgs) Handles bVai.Click
+        Vai()
+    End Sub
+
+    Private Sub bFrom_Click(ByVal sender As Object, ByVal e As EventArgs) Handles bFrom.Click
+        eFrom.Text = ScegliCartella()
+    End Sub
+
+    Private Sub bTo_Click(ByVal sender As Object, ByVal e As EventArgs) Handles bTo.Click
+        eTo.Text = ScegliCartella()
+    End Sub
+
+    Function ScegliCartella() As String
+        Using op As New FolderBrowserDialog
+            If op.ShowDialog() = DialogResult.OK Then
+                Return op.SelectedPath
+            Else
+                Return Nothing
+            End If
+        End Using
+    End Function
+
+    Sub Vai()
+        Dim ok = False
+        Dim erro = "Riempire tutti i campi"
+        Dim f = eFrom.Text
+        Dim t = eTo.Text
+        Dim r = eRename.Text
+
+        Me.Enabled = False
+        Application.DoEvents()
+
+        Try
+            r = r.Replace("\", "")
+            r = r.Replace("/", "")
+            r = r.Replace("?", "")
+            r = r.Replace(":", "")
+            r = r.Replace("*", "")
+            r = r.Replace("<", "")
+            r = r.Replace(">", "")
+            r = r.Replace("|", "")
+        Catch ex As Exception
+            ok = False
+        End Try
+
+        If r <> "" Then
+            If IO.Directory.Exists(f) Then
+                If t <> "" Then
+                    Try
+                        t = t & "\" & r
+                        IO.Directory.CreateDirectory(t)
+                    Catch ex As Exception
+                        erro = ex.Message
+                        ok = False
+                    End Try
+
+                    If IO.Directory.Exists(t) Then
+                        Dim p = IO.Directory.GetFiles(f, "*", IO.SearchOption.AllDirectories)
+                        Dim tt = -1
+                        Dim founded = False
+                        Dim typ(99) As TipoFile
+
+                        For y As Integer = 0 To p.Length - 1
+                            founded = False
+
+                            If Not typ Is Nothing Then
+                                If typ.Length > 0 Then
+                                    For z As Integer = 0 To typ.Length - 1
+                                        If IO.Path.GetExtension(p(y)) = typ(z).Tipo Then
+                                            founded = True
+                                            Exit For
+                                        End If
+                                    Next
+                                End If
+                            End If
+
+                            If founded = False Then
+                                tt += 1
+                                typ(tt) = New TipoFile(IO.Path.GetExtension(p(y)))
+                            End If
+                        Next
+
+                        Dim Type_(tt) As TipoFile
+
+                        Array.Copy(typ, Type_, tt + 1)
+
+                        If Not p Is Nothing Then
+                            If p.Length > 0 Then
+                                Dim s, d As String
+
+                                Me.ProgressBar1.Visible = True
+                                Me.ProgressBar1.Maximum = p.Length
+                                Application.DoEvents()
+
+                                If MsgBox("Vuoi copiare " & p.Length & " file ?", vbQuestion Or vbYesNo) = MsgBoxResult.Yes Then
+                                    Application.DoEvents()
+
+                                    Array.Sort(p)
+
+                                    For x As Integer = 0 To p.Length - 1
+                                        s = p(x)
+                                        d = t & "\" & r & " (" & GetNumberByType(Type_, IO.Path.GetExtension(s)) & ")" & IO.Path.GetExtension(s)
+
+                                        Try
+                                            IO.File.Copy(s, d, False)
+                                        Catch ex As Exception
+                                            ok = False
+                                        End Try
+
+                                        Me.ProgressBar1.Value = x + 1
+                                        Application.DoEvents()
+                                    Next
+
+                                    ok = True
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+        End If
+
+        If ok Then
+            MsgBox("Finito !", vbInformation)
+
+            Try
+                Process.Start(t)
+            Catch ex As Exception
+                Application.DoEvents()
+            End Try
+
+            Spegni()
+        Else
+            Me.Enabled = True
+            MsgBox("Errore : " & erro, vbExclamation)
+        End If
+    End Sub
+
+    Sub PreSpegni()
+        Opzioni = New sOpzioni(eFrom.Text, eTo.Text)
+    End Sub
+
+    Sub Spegni()
+        PreSpegni()
+
+        Application.DoEvents()
+        Me.Close()
+    End Sub
+
+    Function GetNumberByType(ByRef Array_() As TipoFile, ByVal Type_ As String) As Integer
+        Dim i = 0
+
+        If Not Array_ Is Nothing Then
+            If Array_.Length > 0 Then
+                For y As Integer = 0 To Array_.Length - 1
+                    If Array_(y).Tipo = Type_ Then
+                        Array_(y).Numero += 1
+                        i = Array_(y).Numero
+
+                        Exit For
+                    End If
+                Next
+            End If
+        End If
+
+        Return i
+    End Function
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        PreSpegni()
+    End Sub
+
+    Public Function DateToStringWithUnderSpace(ByVal dd As Date) As String
+        Dim d = dd.Day
+        Dim m = dd.Month
+        Dim y = Microsoft.VisualBasic.Right(dd.Year, 2)
+
+        If dd.Day < 10 Then d = "0" & d
+        If dd.Month < 10 Then m = "0" & m
+
+        Dim s = y & "_" & m & "_" & d
+
+        Return s
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim o = Opzioni
+
+        Me.eRename.Text = "_" & DateToStringWithUnderSpace(Now.AddDays(-1))
+        Me.eRename.SelectionStart = 0
+        Me.eRename.SelectionLength = 0
+
+        If IO.Directory.Exists(o.To_) Then Me.eTo.Text = o.To_
+        If IO.Directory.Exists(o.From_) Then Me.eFrom.Text = o.From_
+    End Sub
+
+
+End Class
